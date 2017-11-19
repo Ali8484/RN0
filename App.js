2417f80ab778a62a80a4ff3aac5313ef3d91946e
github_ali8484 <you@domain.com>
2017-11-19 13:15:47 +0300

    'Text_Input_added'

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
-import {AppRegistry,Text,View, StyleSheet ,TouchableHighlight,TouchableOpacity} from 'react-native';
+import {AppRegistry,Text,View, StyleSheet } from 'react-native';
 
 //import Component1 from './app/components/component1/Component1';
-import Component2 from './app/components/component2/Component2';
+import Component3 from './app/components/component3/Component3';
 
 export default class yes extends Component{
 render(){
   return(
     <View> 
-      <Component2 />
+      <Component3 />
        </View>
   );
 }
